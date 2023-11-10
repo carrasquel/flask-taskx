06fe14e7dbc31312a37421f75cefd77f63a5bdc0
Nelson Carrasquel <carrasquel@outlook.com>
2023-11-10 17:37:14 -0400

    base interface added

---
+++ flask_taskx/engines/amqp/core.py
@@ -0,0 +1,18 @@
+
+
+class AMQPWorker:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def initialize_amqp(self, *args, **kwargs):
+        pass
+
+    def create_queues(self, *args, **kwargs):
+        pass
+
+    def task_consumer(self, *args, **kwargs):
+        pass
+
+    def task_producer(self, *args, **kwargs):
+        pass
---
+++ flask_taskx/engines/sql/core.py
@@ -0,0 +1,21 @@
+
+
+class SQLWorker:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def initialize_db(self, *args, **kwargs):
+        pass
+
+    def create_db(self, *args, **kwargs):
+        pass
+
+    def create_tables(self, *args, **kwargs):
+        pass
+
+    def task_consumer(self, *args, **kwargs):
+        pass
+
+    def task_producer(self, *args, **kwargs):
+        pass
